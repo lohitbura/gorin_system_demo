21a3d2f766b72d167261f46dd53e346ccd643eab
lohitbura <lohitbura@gmail.com>
2025-05-13 16:58:23 +0530

    feat: Firebase Initial Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gs_demo/core/firebase/firebase_options.dart';
 import 'package:gs_demo/features/auth_management/pages/login_screen.dart';
 import 'package:gs_demo/features/auth_management/pages/register_screen.dart';
 import 'package:gs_demo/resources/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -52,36 +60,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home:  RegisterScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Text(
-          'Hello GS',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
